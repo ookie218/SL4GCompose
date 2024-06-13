ccf43a2fb2776c943acff232c96ac3fd5f00d55e
Michael Ookie Ebron <ookieebron@gmail.com>
2024-06-13 16:48:15 -0400

    Home Screen Built

--- app/src/main/java/com/ookie/sl4gcompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/ookie/sl4gcompose/ui/screens/HomeScreen.kt
@@ -1,11 +1,39 @@
 package com.ookie.sl4gcompose.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ookie.sl4gcompose.R
+import com.ookie.sl4gcompose.ui.theme.Typography
 
 @Composable
-fun HomeScreen() {
-
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.sl4gministriesdefault),
+            contentDescription = stringResource(id = R.string.group_photo_description),
+            modifier.size(150.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.group_bio),
+            modifier = Modifier.padding(16.dp),
+            style = Typography.bodyMedium
+        )
+    }
 }
 
 @Preview(showBackground = true)
