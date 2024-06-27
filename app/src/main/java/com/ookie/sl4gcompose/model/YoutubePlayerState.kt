f66c33bce74cf77456ae00668d9e42e839ff7411
Michael Ookie Ebron <ookieebron@gmail.com>
2024-06-26 21:22:57 -0400

    Deleted YoutubePlayerSTate.kt interface as the import provides us with all of the states already.

--- app/src/main/java/com/ookie/sl4gcompose/model/YoutubePlayerState.kt
+++
@@ -1,7 +0,0 @@
-package com.ookie.sl4gcompose.model
-
-sealed interface YoutubePlayerState {
-    object Playing : YoutubePlayerState
-    object Buffering : YoutubePlayerState
-    object Ended : YoutubePlayerState
-}
